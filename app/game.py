a66eee47a8ea5d4abf0c0a7234d6d8ebaf6fc2ef
ksimon17 <78188196+ksimon17@users.noreply.github.com>
2022-03-23 17:53:09 -0400

    Game Improvements #1 (#1)
    * Remove duplicated code by creating a list of valid_selections (i.e., simplify valid options)
    
    * Add code climate conf file

--- app/game.py
+++ app/game.py
@@ -1,8 +1,6 @@
-
-
-
-
 from random import choice
+
+valid_selections = ["rock", "paper", "scissors"] # only have to update in one place
 
 #
 # USER SELECTION
@@ -10,7 +8,7 @@ from random import choice
 
 u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
 print("USER CHOICE:", u)
-if u not in ["rock", "paper", "scissors"]:
+if u not in valid_selections:
     print("OOPS, TRY AGAIN")
     exit()
 
@@ -18,7 +16,7 @@ if u not in ["rock", "paper", "scissors"]:
 # COMPUTER SELECTION
 #
 
-c = choice(["rock", "paper", "scissors"])
+c = choice(valid_selections)
 print("COMPUTER CHOICE:", c)
 
 #
