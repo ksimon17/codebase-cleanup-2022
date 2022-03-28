2d22d1a1ed5f737c5147451a603b62a7b6eb1561
ksimon17 <78188196+ksimon17@users.noreply.github.com>
2022-03-28 18:00:31 -0400

    Refactor across files

---
+++ app/utils.py
@@ -0,0 +1,15 @@
+
+
+def to_usd(my_price):
+    """
+    This is a docstring. It tells us what this function is about.
+    What its responsibilities are.
+    What the params are about 
+    What the datatypes the params are. 
+    What will this function will return 
+    Example of invoking the function. 
+    
+    Invoke like this: to_usd(9.9999)
+
+    """
+    return '${:,.2f}'.format(my_price)
