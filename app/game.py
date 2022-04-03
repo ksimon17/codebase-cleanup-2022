56002edd72d4a16271cda94ae02705221ea55dab
ksimon17 <78188196+ksimon17@users.noreply.github.com>
2022-04-03 00:45:06 -0400

    Documenting Refactored Functions with docstrings (#6)

--- app/game.py
+++ app/game.py
@@ -3,6 +3,18 @@ from random import choice
 
 
 def determine_winner(user_choice, computer_choice):
+    """
+    This function determines the winning choice (or lack thereof) of a single "Rock, Paper, Scissors" Game based 
+    on the user's selection and the computer's selection. 
+    Params: The function's two paramters are user_choice, which stores the what choice the user selects in the
+    rock, paper, scissors game, and computer_choice, which stores which choice the computer randomly generates. 
+    Datatypes of Params: user_choice is a String, computer_choice is a String
+    Return: This function returns the winning choice (or lack thereof) amongst the user and computer's selections
+    (either "paper", "rock", "scissors", or None) 
+
+    Invoke the function like this: determine_winner("rock", "paper")
+    """
+    
     winners = {
         "rock": {
             "rock": None,
